d5433300c2bd4479008d42aec677460fce72dd0c
Nick Zent <nickdzent@gmail.com>
2020-05-19 20:35:52 -0700

    add search bar and a little style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { DataTableComponent } from './components/data-table/data-table.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListFilterPipe } from './components/data-table/list-filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataTableComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ListFilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
+import { Doctors } from 'src/app/types/doctors';
 
 @Component({
   selector: 'app-data-table',
@@ -11,13 +12,29 @@ export class DataTableComponent implements OnInit {
 
 ItemsArray= [];
 
-constructor(private dataService: DataServiceService) { }
+constructor(
+    private dataService: DataServiceService,
+          ) { }
+@Input()
+public contact;
 
 ngOnInit() {
   this.dataService.getData().subscribe((res: any[])=>{
     this.ItemsArray= res;
-    console.dir(this.ItemsArray);
   })  
 }
+@Input()
+public doctors:  Doctors[] ;
 
+public currentContact: Doctors;
+
+searchModel: string;
+
+
+@Output() public select: EventEmitter<{}> = new EventEmitter();
+
+public onSelect(doctor: Doctors): void {
+  this.currentContact = doctor; 
+  this.select.emit(doctor);
+}
 }
